c5c9b8a658f40543149b960b395063aa824bdc4f
Krystina <krystinadiao@gmail.com>
2016-08-17 00:18:12 -0500

    Initial release

---
+++ js/test_load.js
@@ -0,0 +1,9 @@
+$(function() {
+
+	// call the function as many times as you need to load charts. Passing the 3 necessary fields that we created in test_functions.js
+
+    buildChartCategories("#barchart", "bar", "../data/barChartCats.json");
+
+
+
+});
